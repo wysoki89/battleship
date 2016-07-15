d594687edbcbbd364f0794666deefeefbda4c1c5
wysoki89 <twysokinski@gmail.com>
2016-07-15 12:42:41 +0200

    example config

---
+++ README.md
@@ -0,0 +1,2 @@
+# Battleship game
+to start game you need to put your database credentials to file config.js - see example in exampleConfig.js
---
+++ exampleConfig.js
@@ -0,0 +1,10 @@
+var config = {
+    //mongodb connection settings
+    database: {
+        login:   'YOUR_LOGIN',
+        password:   'YOUR_PASSWORD',
+        url: '@dsXXXXXX.mlab.com:XXXXX/',
+        dbname: 'DB_NAME'
+    }
+}
+module.exports = config;
